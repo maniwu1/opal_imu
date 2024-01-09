442f4167106d4be4c3fcef73ce717a2ab7df5933
maniwu1 <103662206+maniwu1@users.noreply.github.com>
2024-01-09 12:13:25 -0500

    rename simple_stream folder and create new opal.py using available streaming functions

---
+++ opal.py
@@ -0,0 +1,7 @@
+import sys
+import csv
+from simple_stream import app_logger as AppLogger
+from simple_stream import sensor_config as SensorConfig
+from simple_stream import sensor_stream as SensorStream
+from simple_stream import stream_csv_writer as StreamCsvWriter
+
